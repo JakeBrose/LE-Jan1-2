a787d56e510744a68ccbe3f9737d7d2c8b79355c
JakeBrose <mordecaistudios@gmail.com>
2018-01-02 10:38:13 -0600

    initial commit

---
+++ lightning.js
@@ -0,0 +1,16 @@
+hitchhikers_guide = {
+    characters: ["Zaphod", "Arthur", "Ford", "Trillian"],
+    catchphrase: "Don't Panic",
+    random_facts: {
+        copies_sold: 14000000,
+        formats: ["radio", "TV", "film", "graphic novel"],
+        ultimate_answer: {
+            meaning_of_life: 42
+        }
+    }
+};
+
+console.log(hitchhikers_guide.random_facts.ultimate_answer.meaning_of_life);
+
+let meaning = "meaning_of_life"
+console.log(hitchhikers_guide.random_facts.ultimate_answer[meaning]);
